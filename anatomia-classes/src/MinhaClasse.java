cdabc5f29d8297dc2834569b39e105ffcc11aee2
Paulo <paulo.henrique.chi@outlook.com>
2022-10-26 13:59:21 -0300

    first commit

---
+++ anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Aimer";
+        String segundoNome = "Chi";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
